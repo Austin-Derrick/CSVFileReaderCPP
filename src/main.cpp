63da1f3fb7e4e478321110fb17b6eb3189627079
Austin-Derrick <aderrickson@colum.edu>
2021-04-15 16:55:46 -0500

    made getting information from csv file faster

--- src/main.cpp
+++ src/main.cpp
@@ -7,48 +7,19 @@ int main(){
     std::vector<std::string> people;
     std::ifstream file;
     file.open("../src/people.csv");
-    // file.open("people.txt");
-    std::string name = "";
-    std::string email = "";
-    std::string ID = "";
-    int index = 0;
-    while (file.is_open())
-    {
-        getline(file, name, ',');
-        getline(file, email, ',');
-        getline(file, ID, '\n');
-        people.push_back(name);
-        people.push_back(email);
-        people.push_back(ID);
-        if (index == 0)
-        {
-            index++;
-        }
-        else{
-            file.close();
-
-        }
-        
-    }
-    std::cout<<people[0]<<std::endl;
-    std::cout<<people[1]<<std::endl;
-    std::cout<<people[2]<<std::endl;
-    std::cout<<people[3]<<std::endl;
-    std::cout<<people[4]<<std::endl;
-    std::cout<<people[5]<<std::endl;
-    // if (file.is_open())
-    // {
-    //     std::cout<<"File Is Open"<<std::endl;
-    //     file.close();
-    //     if (!file.is_open())
-    //     {
-    //         std::cout<<"File Is closed"<<std::endl;
-    //     }
-        
-    // }
-    // else if(!file.is_open()){
-
-    //     std::cout<<"File Is not touched"<<std::endl;
-    // }
     
+    if (file.is_open())
+    {
+        std::string name;
+        while (getline(file, name))
+        {
+            people.push_back(name);
+        }
+        file.close();
+    }
+    
+    for (int i = 0; i < people.size();i++)
+    {
+        std::cout<<people[i]<<std::endl;
+    }
 }
