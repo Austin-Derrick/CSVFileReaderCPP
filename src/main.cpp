9599f03273ba2ff4dad8be735c64c6a1bec63e3c
Austin-Derrick <aderrickson@colum.edu>
2021-04-15 15:20:12 -0500

    Added logic for additional lines

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,12 @@
 int main(){
     std::vector<std::string> people;
     std::ifstream file;
-    file.open("../src/people.txt");
+    file.open("../src/people.csv");
     // file.open("people.txt");
     std::string name = "";
     std::string email = "";
     std::string ID = "";
+    int index = 0;
     while (file.is_open())
     {
         getline(file, name, ',');
@@ -19,11 +20,22 @@ int main(){
         people.push_back(name);
         people.push_back(email);
         people.push_back(ID);
-        file.close();
+        if (index == 0)
+        {
+            index++;
+        }
+        else{
+            file.close();
+
+        }
+        
     }
     std::cout<<people[0]<<std::endl;
     std::cout<<people[1]<<std::endl;
     std::cout<<people[2]<<std::endl;
+    std::cout<<people[3]<<std::endl;
+    std::cout<<people[4]<<std::endl;
+    std::cout<<people[5]<<std::endl;
     // if (file.is_open())
     // {
     //     std::cout<<"File Is Open"<<std::endl;
